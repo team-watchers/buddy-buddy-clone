51d1d47ee3d53bb55dd856ffd0ee11cdd8eae5bc
landon <landon@olulo.io>
2020-07-21 18:52:49 +0900

    first commit

---
+++ src/main/java/com/teamwatchers/buddybuddyclone/BuddybuddycloneApplication.java
@@ -0,0 +1,13 @@
+package com.teamwatchers.buddybuddyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddybuddycloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddybuddycloneApplication.class, args);
+	}
+
+}
