3e330fede03bc6cfa976ca0a0a2ff9ed13c65ea8
landon <landon@olulo.io>
2020-07-28 19:08:11 +0900

    Implements Chat room

---
+++ src/main/java/com/teamwatchers/buddybuddyclone/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.teamwatchers.buddybuddyclone;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType {
+        ENTER,
+        TALK
+    }
+
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
+}
