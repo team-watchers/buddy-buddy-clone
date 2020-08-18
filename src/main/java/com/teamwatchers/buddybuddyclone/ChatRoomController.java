fdba8db11a5606852676fd2a6479df5b0d180172
hyeongwon byun <bhw0506@gmail.com>
2020-08-18 18:21:30 +0900

    jtw

--- src/main/java/com/teamwatchers/buddybuddyclone/ChatRoomController.java
+++ src/main/java/com/teamwatchers/buddybuddyclone/ChatRoomController.java
@@ -1,11 +1,12 @@
 package com.teamwatchers.buddybuddyclone;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -15,8 +16,10 @@ public class ChatRoomController {
 
     private final ChatRoomRepository chatRoomRepository;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @GetMapping("/room")
-    public String rooms(Model model) {
+    public String rooms() {
         return "/chat/room";
     }
 
@@ -42,5 +45,13 @@ public class ChatRoomController {
     @ResponseBody
     public ChatRoom roomInfo(@PathVariable String roomId) {
         return chatRoomRepository.findRoomById(roomId);
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public LoginInfo getUserInfo() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        return LoginInfo.builder().name(name).token(jwtTokenProvider.generateToken(name)).build();
     }
 }
